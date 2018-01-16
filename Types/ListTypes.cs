9447740315d18dd6bccd56698f97dbbd2bd69a16
NicholasToogood <toogood-nick-56@hotmail.com>
2018-01-16 09:07:26 -0400

    dem changes

---
+++ Types/ListTypes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types
+{
+    public class ListTypes
+    {
+    }
+
+
+    public struct Parms
+    {
+        public string name;
+        public object value;
+        public ParameterDirection direction;
+        public SqlDbType datatype;
+        public int size;
+        public Parms(string tempName, object tempValue, ParameterDirection tempDirection, SqlDbType tempDatatype, int tempSize = 0)
+        {
+            this.name = tempName;
+            this.value = tempValue;
+            this.direction = tempDirection;
+            this.datatype = tempDatatype;
+            this.size = tempSize;
+        }
+    }
+}
