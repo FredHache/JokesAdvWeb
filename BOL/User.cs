d7e33f1ed8ba9a3db4946c6b8ff0e494b7e99884
NicholasToogood <toogood-nick-56@hotmail.com>
2018-01-16 09:19:24 -0400

    reference Types

--- BOL/User.cs
+++ BOL/User.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace BOL
 {
-    class User
+    class User : Types:IUser
     {
+        internal int _id;
+        internal string _first_name;
+        internal string _last_name;
+        internal string _emailAddress;
+        internal string _username;
+        internal string _password;
+        internal DateTime _created_at;
+        internal DateTime _last_login;
+        internal string _avatar;
+
+        public 
+
     }
 }
