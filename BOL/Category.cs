f0f73a7d243f9203bc6fba2c23ebd03e8794ca8f
NicholasToogood <toogood-nick-56@hotmail.com>
2018-01-16 09:28:56 -0400

    category

--- BOL/Category.cs
+++ BOL/Category.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace BOL
 {
-    class Category
+    class Category : Types.ICategories
     {
+        internal int _categoryID;
+        internal string _categoryName;
+        internal string _description;
+        internal DateTime _createdDate;
+
+        // CATEGORY ID
+        public int categoryID
+        {
+            get { return _categoryID; }
+            set { _categoryID = value; }
+        }
+
+        // CATEGORY NAME
+        public string categoryName
+        {
+            get { return _categoryName; }
+            set { _categoryName = value; }
+        }
+
+        // DESCRIPTION
+        public string description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        // CATEGORY CREATION DATE
+        public DateTime createdDate
+        {
+            get { return _createdDate; }
+            set { _createdDate = value; }
+        }
     }
 }
