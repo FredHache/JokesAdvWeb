331f55bd9fb0ed47a2a49c8414d622b1aa95e4d6
NicholasToogood <toogood-nick-56@hotmail.com>
2018-01-16 09:13:04 -0400

    types changes

--- Types/ListTypes.cs
+++ Types/ListTypes.cs
@@ -10,6 +10,36 @@ namespace Types
     public class ListTypes
     {
     }
+    public interface ICategories
+    {
+        int categoryID { get; set; }
+        string categoryName { get; set; }
+        string description { get; set; }
+        DateTime createdDate { get; set; }
+    }
+    public interface IJokes
+    {
+        int id { get; set; }
+        string title { get; set; }
+        string teaser { get; set; }
+        string joke_text { get; set; }
+        DateTime created_at { get; set; }
+        int user_id { get; set; }
+        int category_id { get; set; }
+        bool featured { get; set; }
+    }
+    public interface IUsers
+    {
+        int id { get; set; }
+        string first_name { get; set; }
+        string last_name { get; set; }
+        string emailAddress { get; set; }
+        string username { get; set; }
+        string password { get; set; }
+        DateTime created_at { get; set; }
+        DateTime last_login { get; set; }
+        string avatar { get; set; }
+    }
 
 
     public struct Parms
