44db0c837e72da2d373c0b46d5306425e9e7d75d
NicholasToogood <toogood-nick-56@hotmail.com>
2018-01-16 10:24:10 -0400

    Joke Class

--- BOL/Joke.cs
+++ BOL/Joke.cs
@@ -8,5 +8,57 @@ namespace BOL
 {
     public class Joke
     {
+        public Joke() { }
+        internal int _id;
+        internal string _title;
+        internal string _teaser;
+        internal string _joke_text;
+        internal DateTime _created_at;
+        internal int _user_id;
+        internal int _category_id;
+        internal bool _featured;
+
+        // JOKE ID
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        
+        public string title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public string teaser {
+            get { return _teaser; }
+            set { _teaser = value; }
+        }
+        public string joke_text
+        {
+            get { return _joke_text; }
+            set { _joke_text = value; }
+        }
+        public DateTime created_at
+        {
+            get { return _created_at; }
+            set { _created_at = value; }
+        }
+
+        public int user_id
+        {
+            get { return _user_id; }
+            set { _user_id = value; }
+        }
+        public int category_id
+        {
+            get { return _category_id; }
+            set { _category_id = value; }
+        }
+        public bool featured
+        {
+            get { return _featured; }
+            set { _featured = value; }
+        }
     }
 }
