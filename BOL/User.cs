90ef9e19ede69a42a2841bf011e06eeffbdb39c3
NicholasToogood <toogood-nick-56@hotmail.com>
2018-01-16 09:24:47 -0400

    Users class

--- BOL/User.cs
+++ BOL/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BOL
 {
-    class User : Types:IUser
+    class User : Types.IUsers
     {
         internal int _id;
         internal string _first_name;
@@ -18,7 +18,68 @@ namespace BOL
         internal DateTime _last_login;
         internal string _avatar;
 
-        public 
 
+        // USER ID
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        // FIRST NAME
+        public string first_name
+        {
+            get { return _first_name; }
+            set { _first_name = value; }
+        }
+
+        // LAST NAME
+        public string last_name
+        {
+            get { return _last_name; }
+            set { _last_name = value; }
+        }
+
+        // EMAIL ADDRESS
+        public string emailAddress
+        {
+            get { return _emailAddress; }
+            set { _emailAddress = value; }
+        }
+
+        // USERNAME
+        public string username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        // PASSWORD
+        public string password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        // ACCOUNT CREATION DATE
+        public DateTime created_at
+        {
+            get { return _created_at; }
+            set { _created_at = value; }
+        }
+
+        // LAST LOGIN
+        public DateTime last_login
+        {
+            get { return _last_login; }
+            set { _last_login = value; }
+        }
+
+        // AVATAR
+        public string avatar
+        {
+            get { return _avatar; }
+            set { _avatar = value; }
+        }
     }
 }
