447a3e8887d8f558d11182299a6e2b820c28ce0b
NicholasToogood <toogood-nick-56@hotmail.com>
2018-01-17 09:42:21 -0400

    comments on jokes

--- BOL/Joke.cs
+++ BOL/Joke.cs
@@ -25,36 +25,48 @@ namespace BOL
             set { _id = value; }
         }
         
+        // TITLE
         public string title
         {
             get { return _title; }
             set { _title = value; }
         }
+
+        // TEASER
         public string teaser {
             get { return _teaser; }
             set { _teaser = value; }
         }
+
+        // JOKE TEXT
         public string joke_text
         {
             get { return _joke_text; }
             set { _joke_text = value; }
         }
+
+        // CREATION DATE
         public DateTime created_at
         {
             get { return _created_at; }
             set { _created_at = value; }
         }
 
+        // USER ID
         public int user_id
         {
             get { return _user_id; }
             set { _user_id = value; }
         }
+
+        // CATEGORY ID
         public int category_id
         {
             get { return _category_id; }
             set { _category_id = value; }
         }
+
+        // FEATURED
         public bool featured
         {
             get { return _featured; }
