23be469dfa76508f01977aa088c106da0259989a
Frederic <frederic.hache@gmail.com>
2018-01-17 09:30:52 -0400

    Create&Update Jokes SQL

--- SQL/SQLMethods.cs
+++ SQL/SQLMethods.cs
@@ -34,8 +34,39 @@ namespace SQL
         }
 
         //Create New Joke
+        public static Boolean CreateJoke(IJokes jokes)
+        {
+            List<Parms> parms = new List<Parms>();
+            parms.Add(new Parms("@id", jokes.id, ParameterDirection.Output, SqlDbType.Int));
+            parms.Add(new Parms("@title", jokes.title, ParameterDirection.Input, SqlDbType.NVarChar, 50));
+            parms.Add(new Parms("@teaser", jokes.teaser, ParameterDirection.Input, SqlDbType.NVarChar, 150));
+            parms.Add(new Parms("@joke_text", jokes.joke_text, ParameterDirection.Input, SqlDbType.NText));
+            parms.Add(new Parms("@created_at", jokes.created_at, ParameterDirection.Input, SqlDbType.Date));
+            parms.Add(new Parms("@user_id", jokes.user_id, ParameterDirection.Input, SqlDbType.Int));
+            parms.Add(new Parms("@category_id", jokes.category_id, ParameterDirection.Input, SqlDbType.Int));
+            parms.Add(new Parms("@featured", jokes.featured, ParameterDirection.Input, SqlDbType.Bit));
 
+            DAL.DataAccessLayer.SendData("createJoke", parms);
+            jokes.category_id = Convert.ToInt32(parms[0].value);
+
+            return true;
+        }
 
         //Update Joke
+        public static Boolean UpdateJoke (IJokes jokes)
+        {
+            List<Parms> parms = new List<Parms>();
+            parms.Add(new Parms("@id", jokes.id, ParameterDirection.Input, SqlDbType.Int));
+            parms.Add(new Parms("@title", jokes.title, ParameterDirection.Input, SqlDbType.NVarChar, 50));
+            parms.Add(new Parms("@teaser", jokes.teaser, ParameterDirection.Input, SqlDbType.NVarChar, 150));
+            parms.Add(new Parms("@joke_text", jokes.joke_text, ParameterDirection.Input, SqlDbType.NText));
+            parms.Add(new Parms("@created_at", jokes.created_at, ParameterDirection.Input, SqlDbType.Date));
+            parms.Add(new Parms("@user_id", jokes.user_id, ParameterDirection.Input, SqlDbType.Int));
+            parms.Add(new Parms("@category_id", jokes.category_id, ParameterDirection.Input, SqlDbType.Int));
+            parms.Add(new Parms("@featured", jokes.featured, ParameterDirection.Input, SqlDbType.Bit));
+
+            DAL.DataAccessLayer.SendData("updateJoke", parms);
+            return true;
+        }
     }
 }
